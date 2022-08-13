cae146732989afccf5b79d8949ec1ac4470c5b67
Rami Elsayed <ramielsayed123@outlook.com>
2022-08-13 19:43:51 +0100

    refactored tests and fixed the bug

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // TODO: Fix the `greet` function so that it returns the correct values.
 function student(name, gradeYear) {
-  let studentInfo = {
-    name: name,
-    gradeYear: gradeYear,
+  const studentInfo = {
+    name,
+    gradeYear,
   };
 
   return {
     greet: function () {
       console.log(
-        `My name is ${this.name} and I am in ${this.gradeYear} grade`
+        `My name is ${studentInfo.name} and I am in ${studentInfo.gradeYear} grade`
       );
     },
   };
 }
 
-const newStudent = student('Dominique', '11th');
-newStudent.greet();
 
 module.exports = student;
