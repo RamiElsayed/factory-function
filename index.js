ff1b5e4013a04d8b9a03eb2fd129bad76e49fddd
Rami Elsayed <ramielsayed123@outlook.com>
2022-08-13 19:45:14 +0100

    refactored function

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // TODO: Fix the `greet` function so that it returns the correct values.
-function student(name, gradeYear) {
+const student = (name, gradeYear) => {
   const studentInfo = {
     name,
     gradeYear,
   };
 
+  const greet = () => {
+    console.log(
+      `My name is ${studentInfo.name} and I am in ${studentInfo.gradeYear} grade`
+    );
+  }
   return {
-    greet: function () {
-      console.log(
-        `My name is ${studentInfo.name} and I am in ${studentInfo.gradeYear} grade`
-      );
-    },
+    greet
   };
 }
 
